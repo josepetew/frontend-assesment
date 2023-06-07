99b95d5970a7c9776b2c63173e0d3e7ed8acf1dc
Jose Peña <josepetew@gmail.com>
2023-06-07 06:54:55 +0200

    fix(slider): fix canvas so it doesn't overflow parent width

--- components/Slider.vue
+++ components/Slider.vue
@@ -26,35 +26,42 @@ export default {
     const images = ref([]);
     const canvasRef = ref(null);
     const sliderRef = ref(null);
-    let dragging = false;
-    let startX = 0;
+    let dragging = ref(false);
+    let startX = ref(0);
     let currentX = ref(0);
-    let totalWidth = ref(0); // Total width of all images combined
+
+    const getClientX = (event) =>
+      event.touches ? event.touches[0].clientX : event.clientX;
+    const toggleDragging = (drag) => {
+      dragging.value = drag;
+      sliderRef.value.classList.toggle("drag-active", drag);
+    };
 
     const startDrag = (event) => {
-      dragging = true;
-      startX =
-        (event.touches ? event.touches[0].clientX : event.clientX) -
-        currentX.value;
-      sliderRef.value.classList.add("drag-active");
+      toggleDragging(true);
+      startX.value = getClientX(event);
     };
 
     const drag = (event) => {
-      if (!dragging) return;
-      const clientX = event.touches ? event.touches[0].clientX : event.clientX;
-      const potentialX = clientX - startX;
-      currentX.value = Math.min(
-        0,
-        Math.max(potentialX, sliderRef.value.clientWidth - totalWidth.value)
+      if (!dragging.value) return;
+      const clientX = getClientX(event);
+      const deltaX = clientX - startX.value;
+      let newCurrentX = currentX.value + deltaX;
+      const minCurrentX = -(
+        sliderRef.value.clientWidth *
+        (props.imagesUrls.length - 1)
       );
+      if (newCurrentX > 0) newCurrentX = 0;
+      if (newCurrentX < minCurrentX) newCurrentX = minCurrentX;
+      currentX.value = newCurrentX;
+      startX.value = clientX;
     };
 
     const endDrag = () => {
-      dragging = false;
-      sliderRef.value.classList.remove("drag-active");
+      toggleDragging(false);
     };
 
-    const loadImage = (url) => {
+    const loadImage = async (url) => {
       return new Promise((resolve, reject) => {
         const img = new Image();
         img.onload = () => resolve(img);
@@ -67,18 +74,27 @@ export default {
       const ctx = canvasRef.value.getContext("2d");
       const width = sliderRef.value.clientWidth;
       const height = sliderRef.value.clientHeight;
-      ctx.clearRect(0, 0, width * props.imagesUrls.length, height);
-      images.value.forEach((img, index) => {
+      ctx.clearRect(0, 0, width, height);
+      let imgIndex = Math.min(
+        Math.floor(Math.abs(currentX.value / width)),
+        props.imagesUrls.length - 1
+      );
+      let nextImgIndex = Math.min(imgIndex + 1, props.imagesUrls.length - 1);
+      let offsetX = currentX.value % width;
+      let drawImage = (img, offset) => {
         let scale = Math.min(
           width / img.naturalWidth,
           height / img.naturalHeight
         );
         let imgWidth = img.naturalWidth * scale;
         let imgHeight = img.naturalHeight * scale;
-        let imgX = index * width + (width - imgWidth) / 2 + currentX.value;
+        let imgX = (width - imgWidth) / 2 + offset;
         let imgY = (height - imgHeight) / 2;
         ctx.drawImage(img, imgX, imgY, imgWidth, imgHeight);
-      });
+      };
+      drawImage(images.value[imgIndex], offsetX);
+      if (nextImgIndex !== imgIndex)
+        drawImage(images.value[nextImgIndex], offsetX + width);
     };
 
     onMounted(async () => {
@@ -86,8 +102,7 @@ export default {
         const img = await loadImage(url);
         images.value.push(img);
       }
-      totalWidth.value = sliderRef.value.clientWidth * props.imagesUrls.length;
-      canvasRef.value.width = totalWidth.value;
+      canvasRef.value.width = sliderRef.value.clientWidth;
       canvasRef.value.height = sliderRef.value.clientHeight;
       renderScene();
     });
@@ -105,7 +120,6 @@ export default {
 .slider-container {
   width: 100%;
   height: 400px;
-  overflow: hidden;
   cursor: grab;
   outline: 1px solid #ccc;
 }
@@ -115,7 +129,6 @@ export default {
 }
 
 .slider-canvas {
-  height: 100%;
   background: rgb(242, 242, 242);
 }
 </style>
